982a728c1bdfc58024e9d21904a57b8c4012caf5
Joe Myers <joecaricature@gmail.com>
2021-06-02 18:43:18 -0400

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ export function App() {
   return (
     <>
       <h1>Classy Color Picker</h1>
+      <div className="Picker">
+        <div>Color</div>
+        <div></div>
+        <div className="Sliders">
+          <label for="Hue">H</label>
+          <input type="range" id="Hue" />
+          <label for="Saturation">S</label>
+          <input type="range" id="Saturation" />
+          <label for="Light">L</label>
+          <input type="range" id="Light" />
+        </div>
+      </div>
     </>
   )
 }
