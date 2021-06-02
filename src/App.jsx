a3c9b99d939e13c01bc30ccd4bfb0e9579471458
Joe Myers <joecaricature@gmail.com>
2021-06-02 19:27:59 -0400

    Messin around with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 
 export function App() {
+  const newBackgroundColor = `hsl(50,8%,20%)`
+  const newStyle = { backgroundColor: newBackgroundColor }
   return (
     <>
       <h1>Classy Color Picker</h1>
       <div className="Picker">
-        <div>Color</div>
-        <div></div>
+        <div className="Color">
+          <div>Color</div>
+          <div className="ColorSwatch" style={newStyle}></div>
+        </div>
         <div className="Sliders">
-          <label for="Hue">H</label>
-          <input type="range" id="Hue" />
+          <label for="Hue">
+            H
+            <input type="range" id="Hue" />
+          </label>
           <label for="Saturation">S</label>
           <input type="range" id="Saturation" />
           <label for="Light">L</label>
           <input type="range" id="Light" />
         </div>
+        <input id="hsla" type="text" />
       </div>
     </>
   )
